bcde81a6aed59d6de5b2149d0618bfa8ad2cee0f
nlilaramani <nrlilaramani@gmail.com>
2020-12-18 20:40:34 -0600

    Initial Project structure

---
+++ src/test/java/com/sel/tests/AShopLoginTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.tests;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author itexps
+ */
+public class AShopLoginTest {
+    
+    public AShopLoginTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    WebDriver driver;
+    @Before
+    public void setUp() {
+       System.setProperty("webdriver.chrome.driver", "c:\\qa\\drivers\\chromedriver.exe");
+       driver=new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+     @Test
+    public void testLogin() throws InterruptedException{
+        driver.get("http://advantageonlineshopping.com/#/");
+        driver.findElement(By.id("hrefUserIcon")).click();
+        driver.findElement(By.name("username")).sendKeys("nltest2");
+        driver.findElement(By.name("password")).sendKeys("Abc@1234");
+        driver.findElement(By.id("sign_in_btnundefined")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[@id='menuUserLink']/span")).click();
+        driver.findElement(By.xpath("//div[@id='loginMiniTitle']/label[3]")).click();
+    }
+}
