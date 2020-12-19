b440a9fa12d467c55b4d2dabc9463ab109829f22
nlilaramani <nrlilaramani@gmail.com>
2020-12-18 21:26:50 -0600

    Initial Project structure

---
+++ src/main/java/com/sel/jenkinssel/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.jenkinssel;
+
+/**
+ *
+ * @author itexps
+ */
+public class Calculator {
+    public int add(int n1, int n2){
+        return n1+n2;
+    }
+    public int sub(int n1,int n2){
+        return n1-n2;
+    }
+    public int multiply(int n1, int n2){
+        return n1*n2;
+    }
+    public int div(int n1,int n2){
+        return n1/n2;
+    }
+}
